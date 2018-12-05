16d6f0f2882853b3c084dac86c257802e0e905ac
Keanneyi Xelhua <kxelhua@tywls.org>
2018-12-05 22:45:48 +0000

    variables and choices

--- js/script.js
+++ js/script.js
@@ -5,11 +5,32 @@
 //GLOBAL VARIABLES
 /* global $ */
 
-
+var userChoice="";
+var computerChoice="";
+var winner="";
+var randomNumber=0;
 
 // DOCUMENT READY FUNCTION BELOW
 $(document).ready(function(){
     $("#shoot").click(function(){
     $("#userChoice").text($("#input").val());
+    userChoice=$("#input").val();
+        $("#userChoice").text(userChoice);
+    randomNumber=Math.Floor(Math.random())*3;
+    computerChoice=$(Math.Floor(Math.random())*3).val();
+        $("#computerChoice").text(computerChoice);
+    if (randomNumber===0){
+        computerChoice="rock";
+        $("#computerChoice").text(computerChoice);
+    } else if (randomNumber===1) {
+        computerChoice="paper";
+        $("#computerChoice").text(computerChoice);
+    } else if (randomNumber===2) {
+        computerChoice="scissors";
+        $("#computerChoice").text(computerChoice);
+    }
 });
 });
+
+
+
